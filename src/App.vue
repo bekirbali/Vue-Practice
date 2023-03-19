452e33d931ada120a73a31fe08d30eb1012b7e17
bekirbali <bfbali43@gmail.com>
2023-03-19 23:55:06 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <HelloWorld count="count" />
+  <button @click="count++">Add 1</button>
+
+  <p v-if="show">{{ title }}: {{ count }}</p>
+  <button @click="changeShow">show-hide</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      title: "I am the count and I am: ",
+      show: true,
+    };
+  },
+  methods: {
+    changeShow() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
